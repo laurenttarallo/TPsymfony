467fe5bcc3fb781a5b4ebb52cd4c4b6051f01831
laurenttarallo tarallo <laurent.tarallo@orange.fr>
2023-10-03 15:28:48 +0000

    public

--- guestbook/src/Controller/ProductsController.php
+++ guestbook/src/Controller/ProductsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ProductRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -9,42 +10,14 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class ProductsController extends AbstractController
 {
-    private array $products = [
-        [
-          'id' => 1,
-          'name' => 'Iphone 13',
-          'price' => 999.99,
-          'description' => 'Apple iPhone 13 Pro 256 Go Bleu Pacifique',
-          'category' => 'smartphone',
-        ],
-        [
-          'id' => 2,
-          'name' => 'Samsung Galaxy S21',
-          'price' => 899.99,
-          'description' => 'Samsung Galaxy S21 5G 128 Go Double SIM Noir Phantom',
-          'category' => 'smartphone',
-        ],
-        [
-          'id' => 3,
-          'name' => 'Huawei P40',
-          'price' => 799.99,
-          'description' => 'Huawei P40 Pro 5G 256 Go Double SIM Noir',
-          'category' => 'smartphone',
-        ],
-        [
-          'id' => 4,
-          'name' => 'MacBook Pro',
-          'price' => 1299.99,
-          'description' => 'Apple MacBook Pro 13" 256 Go SSD 8 Go RAM Intel Core i5 quadricœur à 1,4 GHz Argent',
-          'category' => 'laptop',
-        ]
-        ];
 
     
         #[Route('/products', name: 'app_products')]
-        public function index(): JsonResponse
+        public function index(ProductRepository $productRepository): JsonResponse
         {
-            return new JsonResponse($this->products);
+          return $this->render('product/index.html.twig', [
+            'products' => $productRepository->findAll(),
+          ]);
         }
     
         #[Route('/product/{id}', name: 'app_product')]
